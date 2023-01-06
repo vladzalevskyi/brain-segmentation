77045ec87b0182a1252a0668888f37581f43201c
manasikattel <manasikattel1@gmail.com>
2023-01-06 15:19:07 +0100

    add synthseg channel in the input for the denoiser effect

--- dataset/patch_dataset.py
+++ dataset/patch_dataset.py
@@ -60,17 +60,19 @@ class BrainPatchesDataset(torch.utils.data.Dataset):
                  img_threshold: float = 0.1,
                  denoiser: bool = False,
                  augmentation: bool = False,
-                 normalization: str = 'z_score'):
+                 normalization: str = 'z_score',
+                 root_data_path: str = '/home/user0/misa_vlex/brain_segmentation/data'):
         
         self.split = split
         
         if split == 'train':
-            self.img_dir = Path('./data/Training_Set').resolve()
+            self.img_dir = Path(f'{root_data_path}/Training_Set').resolve()
         if split == 'val':
-            self.img_dir = Path('./data/Validation_Set').resolve()
-        
+            self.img_dir = Path(f'{root_data_path}/Validation_Set').resolve()
+        self.root_data_path = root_data_path
         self.window_size = window_size
         self.stride = stride
+        self.denoiser = denoiser
         self.img_threshold = img_threshold
         self.normalization = normalization
         self.augmentation = augmentation
@@ -101,14 +103,22 @@ class BrainPatchesDataset(torch.utils.data.Dataset):
         self.mask_patches = []
         self.bbox_coords = []
         self.cases = []
-        
+        self.ssegm_patches = []
         
         for dir in self.img_dir.iterdir():
-            if dir.is_dir():
+            if dir.is_dir() and 'unet' not in dir.name:
                 case = dir.name
                 # read image and mask
                 img = sitk.GetArrayFromImage(sitk.ReadImage(str(dir / f'{case}.nii.gz')))
                 segm = sitk.GetArrayFromImage(sitk.ReadImage(str(dir / f'{case}_seg.nii.gz')))
+                
+                if self.denoiser == 'synthseg':
+                    ssegm_path = str(dir / f'{case}_seg.nii.gz')
+                    ssegm_path = ssegm_path.replace('seg.nii.gz', 'seg_resampled.nii.gz')
+                    ssegm_path = ssegm_path.replace('data', 'proc_data')
+                    ssgegm = sitk.GetArrayFromImage(sitk.ReadImage(ssegm_path))
+                    _, ssegm_slices, __ = self.extract_patches(img, ssgegm)
+                    self.ssegm_patches.extend(ssegm_slices)
                 
                 # store slices per image-case
                 img_slices, mask_slices, bbox_coords = self.extract_patches(img, segm)
@@ -169,30 +179,54 @@ class BrainPatchesDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.img_patches)
 
-    def __getitem__(self, idx):
-        img = self.img_patches[idx]
-        
+    def augment(self, img, mask, idx):
+        ssegmask = None
         if self.augmentation and self.split == 'train':
             img = utils.min_max_norm(img, 255).astype('uint8')
             # apply augmentations
-            transformed = self.transform(image=img,
-                                        mask=self.mask_patches[idx])
+            
+            if self.denoiser == False:
+                transformed = self.transform(image=img, mask=mask)
+                
+                mask = transformed['mask']
+            elif self.denoiser == 'synthseg':
+                transformed = self.transform(image=img,
+                                             masks=[mask, self.ssegm_patches[idx]])
+                
+                mask, ssegmask = transformed['masks']
+            else:
+                raise ValueError(f'Denoiser type {self.denoiser} not implemented')
             img = transformed['image'].copy()
-            mask = transformed['mask']
+
         else:
             img = img
-            mask = self.mask_patches[idx]
+            ssegmask = self.ssegm_patches[idx] if self.denoiser == 'synthseg' else None
+        
+        return img, mask, ssegmask
+        
+    def __getitem__(self, idx):
+        img = self.img_patches[idx]
+        
+        
+        img, mask, ssegmask = self.augment(img, self.mask_patches[idx], idx)
             
-        # FOR NOW JUST EXPAND DIMS
-        # LATER COULD ADD ROUGH SEGM
         img = np.expand_dims(img, axis=0)
+        
+        
         if self.normalization == 'min_max':
             img = utils.min_max_norm(img, 1).astype('float32')
         elif self.normalization == 'z_score':
             img = utils.z_score_norm(img, non_zero_region=True)
-               
         
-        return {'img': torch.Tensor(img),
-                'mask': torch.tensor(mask, dtype=torch.long),
+        img = torch.Tensor(img)
+        mask = torch.tensor(mask, dtype=torch.long)
+        
+        # add additional channel for synthseg segmentation
+        if self.denoiser == 'synthseg' and ssegmask is not None:
+            ssegmask = torch.tensor(ssegmask, dtype=torch.float)
+            img = torch.cat([img, ssegmask.unsqueeze(0)], dim=0)
+                
+        return {'img': img,
+                'mask': mask,
                 'bbox': self.bbox_coords[idx],
                 'case': self.cases[idx]}
